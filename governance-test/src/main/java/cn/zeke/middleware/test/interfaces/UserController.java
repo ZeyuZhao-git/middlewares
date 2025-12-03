dca52b7ca49011d9534bf50e19f34a1438aae93b
ZeyuZhao-git <zhaozeyu9185@gmail.com>
2025-12-02 19:40:19 -0500

    governance test

---
+++ governance-test/src/main/java/cn/zeke/middleware/test/interfaces/UserController.java
@@ -0,0 +1,45 @@
+package cn.zeke.middleware.test.interfaces;
+
+import cn.zeke.middleware.governance.annotation.DoHystrix;
+import cn.zeke.middleware.governance.annotation.DoMethodExt;
+import cn.zeke.middleware.governance.annotation.DoRateLimiter;
+import cn.zeke.middleware.governance.annotation.WhitelistFilter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+
+    private Logger logger = LoggerFactory.getLogger(UserController.class);
+
+    /**
+     * 放行：http://localhost:8081/api/queryUserInfo?userId=aaa
+     * 拦截：http://localhost:8081/api/queryUserInfo?userId=bbb
+     */
+    @DoRateLimiter(permitsPerSecond = 0.5, returnJson = "{\"code\":\"1111\",\"info\":\"令牌不足\"}")
+    @WhitelistFilter(key = "userId", returnJson = "{\"code\":\"1111\",\"info\":\"非白名单可访问用户拦截！\"}")
+    @DoHystrix(timeout = 350, returnJson = "{\"code\":\"1111\",\"info\":\"调用方法超过350毫秒，熔断返回！\"}")
+    @DoMethodExt(methodName = "blacklist", returnJson = "{\"code\":\"1111\",\"info\":\"自定义校验方法拦截，不允许访问！\"}")
+    @RequestMapping(path = "/api/queryUserInfo", method = RequestMethod.GET)
+    public UserInfo queryUserInfo(@RequestParam String userId) throws InterruptedException {
+        logger.info("查询用户信息，userId：{}", userId);
+        Thread.sleep(1000);
+        return new UserInfo("虫虫:" + userId, 19, "天津市东丽区万科赏溪苑14-0000");
+    }
+
+    /**
+     * 自定义黑名单，拦截方法
+     */
+    public boolean blacklist(@RequestParam String userId) {
+        if ("aaa".equals(userId) || "222".equals(userId)) {
+            logger.info("拦截自定义黑名单用户 userId：{}", userId);
+            return false;
+        }
+        return true;
+    }
+
+}
